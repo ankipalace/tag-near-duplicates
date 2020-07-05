9014b241513bb3f303b3a1c62585b6a6161b7e3a
Andrew Sanchez <inbox.asanchez@gmail.com>
2020-07-05 11:36:51 -0700

    rm references to df1 and 2

--- tag_dups/cli.py
+++ tag_dups/cli.py
@@ -22,10 +22,9 @@ def fuzzy_dups(df, o_duplicates, threshold, append):
 
 
 @cli.command()
-@click.argument("df1", type=click.Path(exists=True))
-@click.argument("df2", type=click.Path(exists=True))
+@click.argument("df", type=click.Path(exists=True))
 @click.option("--o-duplicates", default="dups.txt", type=click.Path(exists=False))
-def simple_dups(df1, df2, o_duplicates):
+def simple_dups(df, o_duplicates):
     df = read_notes(df)
     df = pre_process(df)
     df = tag_duplicates.simple_dups(df)
