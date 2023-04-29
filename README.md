970a04c4b7990599d3b5763428fa228304682394
Andrew Sanchez <inbox.asanchez@gmail.com>
2023-04-29 13:34:55 -0700

    Update docs.

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
-## Limitations
+# Getting started
+- Create a virtual environment
+- Install requirements: `python -m pip install requirements/requirements.txt`
+- Install the package: `python -m pip install .` (run in the root of this repo)
+- Install required data:  `python -c "import nltk;nltk.download('stopwords')"`
 
-- Currently requires exporting collection to a text file.
+# Usage
+Use the CLI to view usage details:
+
+```
+➜ tag-dups --help
+Usage: tag-dups [OPTIONS] COMMAND [ARGS]...
+
+  Identify duplicates in an plain text file exported from Anki. Save the
+  results to a new plain text file which can be imported back into Anki.
+  Importing this file will add a tag to duplicate notes.
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  fuzzy-dups   Save duplicates identified in DF - a plain text file...
+  simple-dups  Identify exact duplicates identified in DF - a plain text...
+
+➜ tag-dups fuzzy-dups --help
+Usage: tag-dups fuzzy-dups [OPTIONS] DF
+
+  Save duplicates identified in DF - a plain text file exported from Anki
+  using a fuzzy matching algorithm, and save to output file.
+
+Options:
+  --o-duplicates PATH     Path to the output file that will contain notes
+                          tagged as duplicates. Import this file into Anki to
+                          tag duplicate notes.
+  --threshold INTEGER     The strictness threshold. Higher integers will
+                          result in a stricter threshold for determining
+                          duplicates.
+  --append / --no-append  Append results to existing output file.
+  --help                  Show this message and exit.
 
 
-I may eventually turn this into an actual add-on.
+```
--- tag_dups/cli.py
+++ tag_dups/cli.py
@@ -8,15 +8,28 @@ import click
 
 @click.group()
 def cli():
+    """Identify duplicates in an plain text file exported from Anki. Save the results to a new plain text file which can be imported back into Anki.  Importing this file will add a tag to duplicate notes."""
     pass
 
 
 @cli.command()
 @click.argument("df", type=click.Path(exists=True))
-@click.option("--o-duplicates", type=click.Path(exists=False))
-@click.option("--threshold", default=90)
-@click.option("--append/--no-append", default=True)
+@click.option(
+    "--o-duplicates",
+    type=click.Path(exists=False),
+    help="Path to the output file that will contain notes tagged as duplicates. Import this file into Anki to tag duplicate notes.",
+)
+@click.option(
+    "--threshold",
+    type=int,
+    default=90,
+    help="The strictness threshold. Higher integers will result in a stricter threshold for determining duplicates.",
+)
+@click.option(
+    "--append/--no-append", default=True, help="Append results to existing output file."
+)
 def fuzzy_dups(df, o_duplicates, threshold, append):
+    """Save duplicates identified in DF - a plain text file exported from Anki using a fuzzy matching algorithm, and save to output file."""
     if not o_duplicates:
         o_duplicates = f"{Path(df).stem}_threshold_{threshold}.csv"
     df = read_notes(df)
@@ -27,8 +40,14 @@ def fuzzy_dups(df, o_duplicates, threshold, append):
 
 @cli.command()
 @click.argument("df", type=click.Path(exists=True))
-@click.option("--o-duplicates", default="dups.txt", type=click.Path(exists=False))
+@click.option(
+    "--o-duplicates",
+    default="dups.txt",
+    type=click.Path(exists=False),
+    help="Import this file into Anki to tag duplicate notes.",
+)
 def simple_dups(df, o_duplicates):
+    """Identify exact duplicates identified in DF - a plain text file exported from Anki, and save to output file."""
     df = read_notes(df)
     df = pre_process(df)
     df = tag_duplicates.simple_dups(df)
